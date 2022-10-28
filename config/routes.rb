8aba7f925241e6a95677767fbb8d31e26ea56701
Zenji <zenji@ZenjisMacBook-Air.local>
2022-10-28 13:36:30 +0900

    Guest login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users_guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :restaurants
   resources :products do
     resources :votes, only: [:create, :destroy]
